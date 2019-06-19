b6dea3dbec2715b3b5f9204b7a712586b9da1ea2
s-p-n <s-p-n@itendor.com>
2019-06-19 03:36:37 -0600

    Okay, might be usable now

--- test.py
+++ test.py
@@ -196,8 +196,16 @@ tCmd = Thread(target=commandPrompt)
 #tChart = Thread(target=liveChart)
             
 print(f"Please wait, as we are loading {start_q_table}..")
-with open(start_q_table, "rb") as f:
-    q_table = pickle.load(f)
+try:
+    with open(start_q_table, "rb") as f:
+        q_table = pickle.load(f)
+except FileNotFoundError:
+    q_table = {}
+    for i in range(-SIZE+1, SIZE):
+        for ii in range(-SIZE+1, SIZE):
+            for iii in range(-SIZE+1, SIZE):
+                    for iiii in range(-SIZE+1, SIZE):
+                        q_table[((i, ii), (iii, iiii))] = [np.random.uniform(-5, 0) for i in range(9)]
 
 tCmd.start()
 #tChart.start()
